8391b8638b4b1fe9b4e68a99a8c250875a099cea
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-07-07 07:44:52 -0300

    .

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-const mario = document.querySelector(".mario")
-const pipe = document.querySelector(".pipe")
-const nuvem = document.querySelector(".nuvem")
-const startButton = document.querySelector(".start")
-const gameOverScreen = document.querySelector(".game-over")
+ const mario = document.querySelector(".mario")
+ const pipe = document.querySelector(".pipe")
+ const nuvem = document.querySelector(".nuvem")
+ const startButton = document.querySelector(".start")
+ const gameOverScreen = document.querySelector(".game-over")
+ const scoreElement = document.querySelector(".score")
 
+ audioStart = new Audio("/sound/audio_theme.mp3");
+ const gameOverSound = new Audio("./soud/audio_gameover.mp3");
 
-AudioStart = new Audio("/sound/audio_")
-let gameStarted = false;
+ let gameStarted = false;
+ let score = 0;
 
-const startGame = () => {
-    gameStarted = true;
+ const startGame = () => {
+     gameStarted = true;
+     audioStart.play();
 
-    pipe.style.animation = "pipe-animation 1.5s infinite linear";
+     pipe.style.animation = "pipe-animation 1.5s infinite linear";
 
-    startButton.style.display = "none";
-    mario.style.opacity = "1";
-    pipe.style.opacity = "1";
-    nuvem.style.opacity = "1";
+     startButton.style.display = "none";
+     mario.style.opacity = "1";
+     pipe.style.opacity = "1";
+     nuvem.style.opacity = "1";
 }
 
 
-const jump = () => {
-    mario.classList.add("jump");
+ const jump = () => {
+     mario.classList.add("jump");
 
     setTimeout(() => {
         mario.classList.remove("jump");
@@ -29,6 +33,9 @@ const jump = () => {
     ,500);
 }
 
+const updateScore = () => {
+    score += 1;
+    scoreElement.textcontent = score;
 const loop = setInterval(() => {
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
